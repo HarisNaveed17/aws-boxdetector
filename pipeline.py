0bb5dd904a1a5089a4b6de118fe7e66e3cb57114
HarisNaveed17 <haris.naveed174@gmail.com>
2021-07-11 20:03:25 +0500

    Add docstrings

--- pipeline.py
+++ pipeline.py
@@ -4,12 +4,22 @@ from utils import *
 load_dotenv()
 
 class BoxDetector:
-    """[summary]
+    """
+    Pipeline that uses the utility functions to process document image, separate selection elements
+    and find associated text.
     """
     def __init__(self):
         self.client = cloudClientFactory().aws_client()
  
     def run(self, document):
+        """Runs pipeline on the input document image.
+
+        Args:
+            document (.png, .jpeg): Image of scanned document.
+
+        Returns:
+            dict: Selection element text, bounding box and status.
+        """
         output = process_document(document, self.client)
         kmap, vmap, bmap = get_kv_map(output)
         results = get_selection_elements(bmap, vmap, kmap)
